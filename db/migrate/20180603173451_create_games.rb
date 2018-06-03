51943818bcda4198ebb950df63a21977d375411a
GiantSlayer1913 <michaeljaredfisch@gmail.com>
2018-06-03 13:49:47 -0400

    Created scaffold for game user:references

---
+++ db/migrate/20180603173451_create_games.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class CreateGames < ActiveRecord::Migration[5.1]
+  def change
+    create_table :games do |t|
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
